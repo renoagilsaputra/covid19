0377d853929c4ae087f1b6a8731965cf6dccb48a
renoagil87 <renoagilsaputra87@gmail.com>
2020-05-05 12:18:17 +0700

    setTimeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,61 @@
 function indo(url) {
     $.ajaxSetup({ cache: false });
-    $.ajax({
-        type: 'GET',
-        url: url,
-        beforeSend: function (xhr) {
-            xhr.overrideMimeType("text/plain; charset=x-user-defined");
-        },
-        error: function (xhr, status, error) {
-            console.log(xhr);
-        }
-    })
-    .done(function (data) {
-        myData = JSON.parse(data);
-        
-        $('#positif').html(myData[0].positif);
-        $('#sembuh').html(myData[0].sembuh);
-        $('#meninggal').html(myData[0].meninggal);
-        
-        
-    });
+    setTimeout(function(){ 
+        $.ajax({
+            type: 'GET',
+            url: url,
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType("text/plain; charset=x-user-defined");
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+            }
+        })
+        .done(function (data) {
+            myData = JSON.parse(data);
+            
+            $('#positif').html(myData[0].positif);
+            $('#sembuh').html(myData[0].sembuh);
+            $('#meninggal').html(myData[0].meninggal);
+            
+            
+        });
+    }, 1000);
+    
 }
 
 function prov(url) {
     $.ajaxSetup({ cache: false });
-    $.ajax({
-        type: 'GET',
-        url: url,
-        beforeSend: function (xhr) {
-            xhr.overrideMimeType("text/plain; charset=x-user-defined");
-        },
-        error: function (xhr, status, error) {
-            console.log(xhr);
-        }
-    })
-    .done(function (data) {
-        myData = JSON.parse(data);
-        let html = "";
-        let no = 1;
-        for(let i = 0; i < myData.length; i++) {
-            html += "<tr>";
-            html += "<td>"+ no++ +"</td>";
-            html += "<td>"+myData[i]['attributes'].Provinsi+"</td>";
-            html += "<td>"+myData[i]['attributes'].Kasus_Posi+"</td>";
-            html += "<td>"+myData[i]['attributes'].Kasus_Semb+"</td>";
-            html += "<td>"+myData[i]['attributes'].Kasus_Meni+"</td>";
-            html += "</tr>";
-        }
-       
-        $('#data').html(html);
+    setTimeout(function(){
+        $.ajax({
+            type: 'GET',
+            url: url,
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType("text/plain; charset=x-user-defined");
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+            }
+        })
+        .done(function (data) {
+            myData = JSON.parse(data);
+            let html = "";
+            let no = 1;
+            for(let i = 0; i < myData.length; i++) {
+                html += "<tr>";
+                html += "<td>"+ no++ +"</td>";
+                html += "<td>"+myData[i]['attributes'].Provinsi+"</td>";
+                html += "<td>"+myData[i]['attributes'].Kasus_Posi+"</td>";
+                html += "<td>"+myData[i]['attributes'].Kasus_Semb+"</td>";
+                html += "<td>"+myData[i]['attributes'].Kasus_Meni+"</td>";
+                html += "</tr>";
+            }
+           
+            $('#data').html(html);
+            
+        });
+    }, 1000);
 
-       
-
-        
-        
-    });
 }
 
 $(document).ready(function () {
